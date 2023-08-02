79978d25faa6952118cffebfb96d113fdbe58d29
Dan Kiss <flowfast47@gmail.com>
2023-08-02 14:12:40 -0700

    add addPatient and addDoctor actions to controllers with views

--- DoctorPatient/Controllers/DoctorsController.cs
+++ DoctorPatient/Controllers/DoctorsController.cs
@@ -7,32 +7,64 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DoctorPatient.Controllers
 {
-    public class DoctorsController: Controller
+  public class DoctorsController : Controller
+  {
+    private readonly DoctorPatientContext _db;
+    public DoctorsController(DoctorPatientContext db)
     {
-        private readonly DoctorPatientContext _db;
-        public DoctorsController(DoctorPatientContext db)
-        {
-            _db = db;
-        }
-
-        public ActionResult Index()
-        {
-            List<Doctor> model = _db.Doctors.ToList();
-            return View(model);
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(Doctor doctor)
-        {
-            _db.Doctors.Add(doctor);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+      _db = db;
     }
-    
+
+    public ActionResult Index()
+    {
+      List<Doctor> model = _db.Doctors.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Doctor doctor)
+    {
+      _db.Doctors.Add(doctor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Doctor doctorModel = _db.Doctors
+                  .Include(doctor => doctor.JoinEntities)
+                  .ThenInclude(join => join.Patient)
+                  .FirstOrDefault(doctor => doctor.DoctorId == id);
+      return View(doctorModel);
+    }
+
+    public ActionResult AddPatient(int id)
+    {
+      Doctor doctorModel = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      Console.WriteLine(4949);
+      Console.WriteLine(doctorModel);
+      ViewBag.PatientId = new SelectList(_db.Patients, "PatientId", "Name");
+      return View(doctorModel);
+    }
+
+    [HttpPost]
+    public ActionResult AddPatient(Doctor doctor, int patientId)
+    {
+#nullable enable
+      DoctorPatientJoin? joinEntity = _db.DoctorPatientJoins.FirstOrDefault(jointEntry => (jointEntry.DoctorId == doctor.DoctorId && jointEntry.PatientId == patientId));
+#nullable disable
+      if (joinEntity == null && patientId != 0)
+      {
+        _db.DoctorPatientJoins.Add(new DoctorPatientJoin() { DoctorId = doctor.DoctorId, PatientId = patientId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = doctor.DoctorId });
+    }
+  }
+
 }
--- DoctorPatient/Controllers/PatientsController.cs
+++ DoctorPatient/Controllers/PatientsController.cs
@@ -35,7 +35,37 @@ namespace DoctorPatient.Controllers
       return RedirectToAction("Index");
     }
 
-    
+    public ActionResult Details(int id)
+    {
+      Patient foundPatient = _db.Patients
+          .Include(patient => patient.JoinEntities)
+          .ThenInclude(join => join.Doctor)
+          .FirstOrDefault(patient => patient.PatientId == id);
+      return View(foundPatient);
+    }
+
+    public ActionResult AddDoctor(int id)
+    {
+      Patient foundPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
+      // List<Doctor> allDoctors = _db.Doctors.ToList();
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View(foundPatient);
+    }
+
+    [HttpPost]
+    public ActionResult AddDoctor(Patient patient, int doctorId)
+    {
+      #nullable enable
+      DoctorPatientJoin? joinEntity = _db.DoctorPatientJoins.FirstOrDefault(join => (join.DoctorId == doctorId && join.PatientId == patient.PatientId));
+      #nullable disable
+      if(joinEntity ==null && doctorId != 0)
+      {
+        _db.DoctorPatientJoins.Add(new DoctorPatientJoin() { DoctorId = doctorId, PatientId = patient.PatientId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = patient.PatientId});
+    }
+
 
 
   }
